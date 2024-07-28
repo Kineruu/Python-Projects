6c872ed0619216eb9e457a2af35a76d164afe32d
Kineruu <Kineru08@gmail.com>
2024-07-28 02:45:07 +0200

    Main and readme
    Added main.py and readme for the future I guess

--- Notifications/main.py
+++ Notifications/main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+from winotify import Notification
+
+Title = input("Title: ")
+Content = input("Content: ")
+icon = input("Any icon? [Coming soon]: ")
+
+Noti = Notification(
+    app_id = "Notification program",
+    title = Title,
+    msg = Content,
+    duration = "short"
+)
+
+Noti.show()
