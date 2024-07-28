9eee4d89f457e191283afc447778f6758a71d1a2
Kineruu <Kineru08@gmail.com>
2024-07-28 06:22:06 +0200

    Updated version of main.py and changed readme
    I added GUI to main.py and main text.py as a normal old version in case I'll need one for some reason. Also read me was updated.

--- Notifications/main.py
+++ Notifications/main.py
@@ -1,32 +1,71 @@
-from winotify import Notification, audio
+from winotify import Notification
+
+import customtkinter as ct
 import datetime
 import time
+import threading
 
 def GetCurrentTime():
     now = datetime.datetime.now()
     return now.strftime("%d.%m.%y"), now.strftime("%H:%M")
 
 Date, Hour = GetCurrentTime()
+now = datetime.datetime.now()
 
-AtWhatDate = input("[DATE]: ")
-AtWhatHour = input("[HOUR]: ")
+ct.set_appearance_mode("dark")
+ct.set_default_color_theme("dark-blue")
 
-Title = input("Title: ")
-Content = input("Content: ")
+Window = ct.CTk()
+Window.geometry("450x350")
 
-time.sleep(2)
+Window.title("                                               Notification Program")
 
-while True:
-    Date, Hour = GetCurrentTime()
-    if Date == AtWhatDate and Hour == AtWhatHour:
-        print("LETSGO")
-        Noti = Notification(
-            app_id="Notification program",
-            title=Title,
-            msg=Content,
-            duration="short"
-        )
+Frame = ct.CTkFrame(master=Window)
+Frame.pack(fill="both", expand=True)
+
+Label = ct.CTkLabel(master=Frame, text=GetCurrentTime())
+Label.pack(pady=10, padx=10)
+
+Entry1 = ct.CTkEntry(master=Frame, placeholder_text="Date")
+Entry1.pack(pady=10, padx=10)
+
+Entry2 = ct.CTkEntry(master=Frame, placeholder_text="Hour")
+Entry2.pack(pady=10, padx=10)
+
+Entry3 = ct.CTkEntry(master=Frame, placeholder_text="Title")
+Entry3.pack(pady=10, padx=10)
+
+Entry4 = ct.CTkEntry(master=Frame, placeholder_text="Content")
+Entry4.pack(pady=10, padx=10)
+
+def check_time(entry_date, entry_hour, entry_title, entry_content, duration):
+    while True:
+        current_date, current_hour = GetCurrentTime()
+        if current_date == entry_date and current_hour == entry_hour:
+            Noti = Notification(
+                app_id="Notification program",
+                title=entry_title,
+                msg=entry_content,
+                duration=duration.lower()
+            )
+            Noti.show()
+            break
         
-        Noti.show()
-        break
-    time.sleep(50)
+        time.sleep(20)
+
+def get_entry(WhateverIsThis=None):
+    entry_date = Entry1.get()
+    entry_hour = Entry2.get()
+    entry_title = Entry3.get()
+    entry_content = Entry4.get()
+    duration = Combobox.get()
+
+    threading.Thread(target=check_time, args=(entry_date, entry_hour, entry_title, entry_content, duration), daemon=True).start()
+
+Combobox = ct.CTkComboBox(Frame, values=["Short (recommended)", "Long"], command=get_entry)
+Combobox.pack(pady=10, padx=10)
+
+Button = ct.CTkButton(master=Frame, text="Confirm", command=get_entry)
+Button.pack(pady=10, padx=10)
+
+Window.mainloop()
