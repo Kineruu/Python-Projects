4118a80f8e5125b760207f34546321406cd87870
Kineruu <Kineru08@gmail.com>
2024-07-28 03:48:06 +0200

    Updated main.py code
    The code should be working now, as in a normal input text code, making GUI now. Also don't forget to drink water.

--- Notifications/main.py
+++ Notifications/main.py
@@ -1,14 +1,32 @@
-from winotify import Notification
+from winotify import Notification, audio
+import datetime
+import time
+
+def GetCurrentTime():
+    now = datetime.datetime.now()
+    return now.strftime("%d.%m.%y"), now.strftime("%H:%M")
+
+Date, Hour = GetCurrentTime()
+
+AtWhatDate = input("[DATE]: ")
+AtWhatHour = input("[HOUR]: ")
 
 Title = input("Title: ")
 Content = input("Content: ")
-icon = input("Any icon? [Coming soon]: ")
 
-Noti = Notification(
-    app_id = "Notification program",
-    title = Title,
-    msg = Content,
-    duration = "short"
-)
+time.sleep(2)
 
-Noti.show()
+while True:
+    Date, Hour = GetCurrentTime()
+    if Date == AtWhatDate and Hour == AtWhatHour:
+        print("LETSGO")
+        Noti = Notification(
+            app_id="Notification program",
+            title=Title,
+            msg=Content,
+            duration="short"
+        )
+        
+        Noti.show()
+        break
+    time.sleep(50)
