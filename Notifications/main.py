86effc46a706bca89d3b1894248d71855e7fd086
Kineruu <Kineru08@gmail.com>
2024-07-30 04:42:20 +0200

    Added sound and fixed entries
    Now you have default audio (If you have one even enabled (system sound) - I don't have it enabled for example) and now whatever you type in the entry and click the button next to it it'll reset it and do what the button should do.

--- Notifications/main.py
+++ Notifications/main.py
@@ -1,4 +1,4 @@
-from winotify import Notification
+from winotify import Notification, audio
 from clipboard import paste
 
 import customtkinter as ct
@@ -10,14 +10,11 @@ def GetCurrentTime():
     now = datetime.datetime.now()
     return now.strftime("%d.%m.%y"), now.strftime("%H:%M")
 
-Date, Hour = GetCurrentTime()
-now = datetime.datetime.now()
-
 ct.set_appearance_mode("dark")
 ct.set_default_color_theme("dark-blue")
 
 Window = ct.CTk()
-Window.geometry("370x250")
+Window.geometry("370x300")
 Window.resizable(False, False)
 
 Window.title("              Notification Program")
@@ -43,6 +40,7 @@ ContentEntry.pack(pady=4)
 def CurrentDate():
     now = datetime.datetime.now()
     NowDate = now.strftime("%d.%m.%y")
+    DateEntry.delete(0, ct.END)
     DateEntry.insert(0, NowDate)
 
 CurrentDateButton = ct.CTkButton(master=Frame, text="Current Date", command=CurrentDate, width=40)
@@ -51,6 +49,7 @@ CurrentDateButton.place(x=260, y=40)
 def CurrentHour():
     now = datetime.datetime.now()
     NowHour = now.strftime("%H:%M")
+    HourEntry.delete(0, ct.END)
     HourEntry.insert(0, NowHour)
 
 CurrentHourButton = ct.CTkButton(master=Frame, text="Current Hour", command=CurrentHour, width=40)
@@ -58,6 +57,7 @@ CurrentHourButton.place(x=260, y=77)
 
 def Paste1():
     UserPaste = paste()
+    TitleEntry.delete(0, ct.END)
     TitleEntry.insert(0, UserPaste)
 
 PasteButton1 = ct.CTkButton(master=Frame, text="Paste", command=Paste1, width=85)
@@ -65,6 +65,7 @@ PasteButton1.place(x=260, y=112)
 
 def Paste2():
     UserPaste = paste()
+    ContentEntry.delete(0, ct.END)
     ContentEntry.insert(0, UserPaste) 
 
 PasteButton2 = ct.CTkButton(master=Frame, text="Paste", command=Paste2, width=85)
@@ -73,21 +74,24 @@ PasteButton2.place(x=260, y=148)
 def check_time(entry_date, entry_hour, entry_title, entry_content):
     while True:
         current_date, current_hour = GetCurrentTime()
-        if current_date == entry_date and current_hour == entry_hour and entry_title != "" or entry_content != "":
+        if current_date == entry_date and current_hour == entry_hour and entry_title != "" and entry_content != "":
+            print("Triggering notification...")
             Noti = Notification(
                 app_id="Notification program",
                 title=entry_title,
                 msg=entry_content,
                 duration="short"
             )
+
+            Noti.set_audio(audio.Default, loop=False)
             Noti.show()
             break
-        else:
 
+        else:
             TextLabel = ct.CTkLabel(master=Frame, text="Something's wrong \nCheck all entries...")
             TextLabel.place(x=4 , y=80)
 
-        time.sleep(20)
+        time.sleep(5)
 
 def get_entry(WhateverIsThis=None):
     entry_date = DateEntry.get()
