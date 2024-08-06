5a2ea4b29bb792e22c73ecf152918f882eeaf161
Kineruu <Kineru08@gmail.com>
2024-08-06 05:54:05 +0200

    Text input Calculator
    By using "simpleeval" module/library, I can put multiple stuff in equation and it'll work completely fine.

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,1 +1,13 @@
-# Hello
+import simpleeval
+
+def Calc(Equations):
+    try:
+        Equations.replace("^", "**")
+        return simpleeval.simple_eval(Equations)
+    except Exception as e:
+        return e
+
+UsersInput = input()
+result = Calc(UsersInput)
+print(result)
+
