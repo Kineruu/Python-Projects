6a5156c6d2b34c290cb5eb8376788d65cb3d0f2e
Kineruu <Kineru08@gmail.com>
2024-08-06 05:15:05 +0200

    Documented the code
    I think the code is well documented for me so whenever I'll have a look at it, I'll know what's going on

--- Notifications/main.py
+++ Notifications/main.py
@@ -6,18 +6,18 @@ import datetime
 import time
 import threading
 
-def GetCurrentTime():
+def GetCurrentTime(): # Getting current date and hour, output should be like this: dd.mm.yyy HH:MM
     now = datetime.datetime.now()
     return now.strftime("%d.%m.%y"), now.strftime("%H:%M")
 
-def GetCurrentHour():
+def GetCurrentHour(): # Getting current hour without minutes
     now = datetime.datetime.now()
     CurrentHour = now.hour
     return CurrentHour
 
 Hour = GetCurrentHour()
 
-if Hour <= 6:
+if Hour <= 6: # If Hour is less than 6 then you'll have dark mode, if it's past 6 AM then you'll have light mode
     ct.set_appearance_mode("dark")
     ct.set_default_color_theme("dark-blue")
 else:
@@ -25,80 +25,79 @@ else:
     ct.set_default_color_theme("green")
 
 
-Window = ct.CTk()
-Window.geometry("370x300")
-Window.resizable(False, False)
+Window = ct.CTk() # Setting up the main window
+Window.geometry("370x300") # Size of it
+Window.resizable(False, False) # Not resizable
 
-Window.title("              Notification Program")
+Window.title("              Notification Program") # The title, the text that appears at the top of the window, there are spaces because if it wouldn't be centered without them.
 
-Frame = ct.CTkFrame(master=Window)
+Frame = ct.CTkFrame(master=Window) # Creating the window itself
 Frame.pack(fill="both", expand=True)
 
-Label = ct.CTkLabel(master=Frame, text=GetCurrentTime())
+Label = ct.CTkLabel(master=Frame, text=GetCurrentTime()) # Date and hour at the top of the Frame
 Label.pack(pady=4)
 
-DateEntry = ct.CTkEntry(master=Frame, placeholder_text="Date")
+DateEntry = ct.CTkEntry(master=Frame, placeholder_text="Date") # Letting the user to put at what date you want the notification to be at. It requires the program to be in the background, if you shut down your device, the notification will NOT show up.
 DateEntry.pack(pady=4)
 
-HourEntry = ct.CTkEntry(master=Frame, placeholder_text="Hour")
+HourEntry = ct.CTkEntry(master=Frame, placeholder_text="Hour") # Same goes for hours
 HourEntry.pack(pady=4)
 
-TitleEntry = ct.CTkEntry(master=Frame, placeholder_text="Title")
+TitleEntry = ct.CTkEntry(master=Frame, placeholder_text="Title") # Self explanatory
 TitleEntry.pack(pady=4)
 
-ContentEntry = ct.CTkEntry(master=Frame, placeholder_text="Content")
+ContentEntry = ct.CTkEntry(master=Frame, placeholder_text="Content") # Self explanatory
 ContentEntry.pack(pady=4)
 
-def CurrentDate():
+def CurrentDate(): # Getting current date, delete and insert are here because if we press the button it'll clear whatever is in the Entry box and will paste the current date
     now = datetime.datetime.now()
     NowDate = now.strftime("%d.%m.%y")
     DateEntry.delete(0, ct.END)
     DateEntry.insert(0, NowDate)
 
-CurrentDateButton = ct.CTkButton(master=Frame, text="Current Date", command=CurrentDate, width=92)
+CurrentDateButton = ct.CTkButton(master=Frame, text="Current Date", command=CurrentDate, width=92) # Current Date button
 CurrentDateButton.place(x=260, y=40)
 
-def CurrentHour():
+def CurrentHour(): # Same thing for the hour
     now = datetime.datetime.now()
     NowHour = now.strftime("%H:%M")
     HourEntry.delete(0, ct.END)
     HourEntry.insert(0, NowHour)
 
-CurrentHourButton = ct.CTkButton(master=Frame, text="Current Hour", command=CurrentHour, width=92)
+CurrentHourButton = ct.CTkButton(master=Frame, text="Current Hour", command=CurrentHour, width=92) # Current Hour button
 CurrentHourButton.place(x=260, y=77)
 
-def Paste1():
+def PasteTitleDef(): # Self explanatory
     UserPaste = paste()
     TitleEntry.delete(0, ct.END)
     TitleEntry.insert(0, UserPaste)
 
-PasteButton1 = ct.CTkButton(master=Frame, text="Paste title", command=Paste1, width=92)
-PasteButton1.place(x=260, y=112)
+PasteTitle = ct.CTkButton(master=Frame, text="Paste title", command=PasteTitleDef, width=92) # Paste Title button
+PasteTitle.place(x=260, y=112)
 
-def Paste2():
+def PasteContentDef(): # Self explanatory
     UserPaste = paste()
     ContentEntry.delete(0, ct.END)
     ContentEntry.insert(0, UserPaste) 
 
-PasteButton2 = ct.CTkButton(master=Frame, text="Paste content", command=Paste2, width=88)
-PasteButton2.place(x=260, y=148)
+PasteContent = ct.CTkButton(master=Frame, text="Paste content", command=PasteContentDef, width=88) # Paste Content button
+PasteContent.place(x=260, y=148)
 
-WarningLabel = ct.CTkLabel(master=Frame, text="If the entries boxes are empty, \nthe notification will NOT show up.")
+WarningLabel = ct.CTkLabel(master=Frame, text="If the entries boxes are empty, \nthe notification will NOT show up.") # Warning for people who think that putting empty entries will do something
 WarningLabel.place(x=90, y=221)
 
-def check_time(entry_date, entry_hour, entry_title, entry_content):
-    while True:
-        current_date, current_hour = GetCurrentTime()
-        if current_date == entry_date and current_hour == entry_hour and entry_title != "" and entry_content != "":
-            print("Triggering notification...")
-            Noti = Notification(
+def check_time(entry_date, entry_hour, entry_title, entry_content): # Getting all requirements that winotify (The module/library that shows notification) needs
+    while True: # Yes, it'll always run in the background as long as you have it opened and it'll check if it's correct date/hour to send the notification every 5 seconds
+        current_date, current_hour = GetCurrentTime() # now.strftime("%d.%m.%y"), now.strftime("%H:%M")
+        if current_date == entry_date and current_hour == entry_hour and entry_title != "" and entry_content != "": # Checking if all entries are filled and not empty
+            Noti = Notification( # Self explanatory
                 app_id="Notification program",
                 title=entry_title,
                 msg=entry_content,
-                duration="short"
+                duration="short" # "Short" because it's long enough for everyone to read the notification content
             )
 
-            Noti.set_audio(audio.Default, loop=False)
+            Noti.set_audio(audio.Default, loop=False) # Audio for people who have system sounds enabled
             Noti.show()
             break
 
@@ -108,14 +107,14 @@ def check_time(entry_date, entry_hour, entry_title, entry_content):
         time.sleep(5)
 
 def get_entry(WhateverIsThis=None):
-    entry_date = DateEntry.get()
+    entry_date = DateEntry.get() # Getting everything that is in the Entries and putting them into the notification.
     entry_hour = HourEntry.get()
     entry_title = TitleEntry.get()
     entry_content = ContentEntry.get()
     
-    threading.Thread(target=check_time, args=(entry_date, entry_hour, entry_title, entry_content), daemon=True).start()
+    threading.Thread(target=check_time, args=(entry_date, entry_hour, entry_title, entry_content), daemon=True).start() 
 
-Button = ct.CTkButton(master=Frame, text="Confirm", command=get_entry)
+Button = ct.CTkButton(master=Frame, text="Confirm", command=get_entry) # Confirm button, what else would you expect
 Button.pack(pady=10, padx=10)
 
 Window.mainloop()
