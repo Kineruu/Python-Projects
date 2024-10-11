d94d421ff5db7b77c3ebc1f3c26254d39cbc390b
Kineruu <Kineru08@gmail.com>
2024-10-11 21:25:44 +0200

    UPDATED main.py AND ADDED Number.txt FILES
    After 2 months of break, I'm finally back.

--- Notifications/main.py
+++ Notifications/main.py
@@ -5,6 +5,7 @@ import customtkinter as ct
 import datetime
 import time
 import threading
+import os
 
 def GetCurrentTime(): # Getting current date and hour, output should be like this: dd.mm.yyy HH:MM
     now = datetime.datetime.now()
@@ -17,16 +18,12 @@ def GetCurrentHour(): # Getting current hour without minutes
 
 Hour = GetCurrentHour()
 
-if Hour <= 6: # If Hour is less than 6 then you'll have dark mode, if it's past 6 AM then you'll have light mode
-    ct.set_appearance_mode("dark")
-    ct.set_default_color_theme("dark-blue")
-else:
-    ct.set_appearance_mode("light")
-    ct.set_default_color_theme("green")
+ct.set_appearance_mode("dark")
+ct.set_default_color_theme("dark-blue")
 
 
 Window = ct.CTk() # Setting up the main window
-Window.geometry("370x300") # Size of it
+Window.geometry("500x300") # Size of it
 Window.resizable(False, False) # Not resizable
 
 Window.title("              Notification Program") # The title, the text that appears at the top of the window, there are spaces because if it wouldn't be centered without them.
@@ -56,7 +53,7 @@ def CurrentDate(): # Getting current date, delete and insert are here because if
     DateEntry.insert(0, NowDate)
 
 CurrentDateButton = ct.CTkButton(master=Frame, text="Current Date", command=CurrentDate, width=92) # Current Date button
-CurrentDateButton.place(x=260, y=40)
+CurrentDateButton.place(x=325, y=40)
 
 def CurrentHour(): # Same thing for the hour
     now = datetime.datetime.now()
@@ -65,7 +62,7 @@ def CurrentHour(): # Same thing for the hour
     HourEntry.insert(0, NowHour)
 
 CurrentHourButton = ct.CTkButton(master=Frame, text="Current Hour", command=CurrentHour, width=92) # Current Hour button
-CurrentHourButton.place(x=260, y=77)
+CurrentHourButton.place(x=325, y=77)
 
 def PasteTitleDef(): # Self explanatory
     UserPaste = paste()
@@ -73,7 +70,7 @@ def PasteTitleDef(): # Self explanatory
     TitleEntry.insert(0, UserPaste)
 
 PasteTitle = ct.CTkButton(master=Frame, text="Paste title", command=PasteTitleDef, width=92) # Paste Title button
-PasteTitle.place(x=260, y=112)
+PasteTitle.place(x=325, y=112)
 
 def PasteContentDef(): # Self explanatory
     UserPaste = paste()
@@ -81,10 +78,31 @@ def PasteContentDef(): # Self explanatory
     ContentEntry.insert(0, UserPaste) 
 
 PasteContent = ct.CTkButton(master=Frame, text="Paste content", command=PasteContentDef, width=88) # Paste Content button
-PasteContent.place(x=260, y=148)
+PasteContent.place(x=325, y=148)
 
-WarningLabel = ct.CTkLabel(master=Frame, text="If the entries boxes are empty, \nthe notification will NOT show up.") # Warning for people who think that putting empty entries will do something
-WarningLabel.place(x=90, y=221)
+#WarningLabel = ct.CTkLabel(master=Frame, text="If the entries boxes are empty, \nthe notification will NOT show up.") # Warning for people who think that putting empty entries will do something
+#WarningLabel.place(x=90, y=221)
+
+with open("C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\Number.txt") as f:
+    NumberFile = int(f.read())
+
+with open(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{NumberFile}.txt", "r") as GetTitle:
+    GetNotificationTitle = GetTitle.readline(1)     
+    GetNotificationTitle = GetNotificationTitle[7:] 
+    GetTitle.close()
+
+def LatestNotification():
+    os.system(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{NumberFile}.txt")
+
+def PreviousNotification():
+    os.system(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{NumberFile-1}.txt")
+
+LatestNotificationButton = ct.CTkButton(master=Frame, text=f"{GetNotificationTitle}", command=LatestNotification, width=60, height=35)
+LatestNotificationButton.place(x=5, y=10)
+
+PreviousNotificationButton = ct.CTkButton(master=Frame, text=f"Notification{NumberFile-1}", command=PreviousNotification, width=40, height=35)
+PreviousNotificationButton.place(x=15, y=50)
+
 
 def check_time(entry_date, entry_hour, entry_title, entry_content): # Getting all requirements that winotify (The module/library that shows notification) needs
     while True: # Yes, it'll always run in the background as long as you have it opened and it'll check if it's correct date/hour to send the notification every 5 seconds
@@ -96,14 +114,24 @@ def check_time(entry_date, entry_hour, entry_title, entry_content): # Getting al
                 msg=entry_content,
                 duration="short" # "Short" because it's long enough for everyone to read the notification content
             )
+            with open("Number.txt", "r+") as f:
+                Number = int(f.read().strip())
+                Number += 1
+                f.seek(0) 
+                f.write(str(Number))
+                f.truncate()
+                with open(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{Number}.txt", "w") as f:
+                    f.write(f"TITLE: {entry_title}\nCONTENT: {entry_content}\nDATE: {entry_date}\nHOUR: {entry_hour}")
+                    f.close()
+
+            LatestNotificationButton.configure(text=f"Notification{Number}", command=lambda: os.system(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{Number}.txt"))
+            PreviousNotificationButton.configure(text=f"Notification{Number-1}", command=lambda: os.system(f"C:\\Users\\Me\\Desktop\\Programming-Projects\\Python-Projects\\NotificationsList\\Notification{Number-1}.txt"))
 
             Noti.set_audio(audio.Default, loop=False) # Audio for people who have system sounds enabled
             Noti.show()
             break
-
         else:
             pass
-
         time.sleep(5)
 
 def get_entry(WhateverIsThis=None):
