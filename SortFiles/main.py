111785cf082cedf2c3f25243b31cbc37105449da
Kineruu <Kineru08@gmail.com>
2024-11-16 21:44:04 +0100

    Added some files and moved my code to a function, planning to add more soon

--- SortFiles/main.py
+++ SortFiles/main.py
@@ -4,32 +4,33 @@ import os
 BasePath = os.path.dirname(os.path.abspath(__file__))
 FilesFolder = os.path.join(BasePath, "test")
 
-try:
-    files = os.listdir(FilesFolder)
+def renameByDate(folder):
+    try:
+        files = os.listdir(folder)
 
-    for item in files:
-        itemPath = os.path.join(FilesFolder, item)
+        for item in files:
+            itemPath = os.path.join(folder, item)
 
-        if os.path.isfile(itemPath):
-            createdTime = os.path.getctime(itemPath)
-            readableTime = time.strftime("%d-%m-%Y %H-%M-%S", time.localtime(createdTime))
+            if os.path.isfile(itemPath):
+                createdTime = os.path.getctime(itemPath)
+                readableTime = time.strftime("%d-%m-%Y %H-%M-%S", time.localtime(createdTime))
 
-            fileExtension = os.path.splitext(item)[1]
-            newItemName = f"{readableTime}{fileExtension}"
-            newPath = os.path.join(FilesFolder, newItemName)
+                fileExtension = os.path.splitext(item)[1]
+                newItemName = f"{readableTime}{fileExtension}"
+                newPath = os.path.join(folder, newItemName)
 
-            number = 1
-            while os.path.exists(newPath):
-                newItemName = f"{readableTime}-{number}{fileExtension}"
-                newPath = os.path.join(FilesFolder, newItemName)
-                number += 1
+                number = 1
+                while os.path.exists(newPath):
+                    newItemName = f"{readableTime}-{number}{fileExtension}"
+                    newPath = os.path.join(folder, newItemName)
+                    number += 1
 
-            os.rename(itemPath, newPath)
-            print(f"Renamed: {item} -> {newItemName}")
+                os.rename(itemPath, newPath)
+                print(f"Renamed: {item} -> {newItemName}")
 
-    print(readableTime)
+    except FileNotFoundError:
+        print(f"The file {item} does not exists!")
+    except Exception as e:
+        print(f"Error: {e}")
 
-except FileNotFoundError:
-    print(f"The file {item} does not exists!")
-except Exception as e:
-    print(f"Error: {e}")
+renameByDate(FilesFolder)
