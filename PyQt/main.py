f52c9fed2607dc5ce21cad868c0c09489c2dec44
Kineruu <Kineru08@gmail.com>
2024-12-15 22:29:49 +0100

    Creating yet another folder - for practicing the new GUI library that I'll be most likely switching to

--- PyQt/main.py
+++ PyQt/main.py
@@ -1,9 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QLabel,  QWidget
+
 import sys
 
-app = QApplication(sys.argv)
+
+app = QApplication([])
 
 window = QWidget()
+window.setWindowTitle("Hello")
+window.setGeometry(50,50,1800,500)
+
+label = QLabel("Hello", parent=window)
+anotherlabel = QLabel("<h4>WAIT I CAN USE HTML AS WELL????????</h4>", parent=window)
+
 window.show()
 
 app.exec()
