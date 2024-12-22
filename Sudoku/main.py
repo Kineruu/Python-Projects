b15eb27e3910979e797c60c871d1b40b92449f50
Kineruu <Kineru08@gmail.com>
2024-12-22 05:10:39 +0100

    Sudoku

---
+++ Sudoku/main.py
@@ -0,0 +1,42 @@
+
+
+# No clue if I'll use png or write a gui for it
+# Imagine having 9x9 board with 3x3 subgrids using Entry boxes hahahahhahah
+# That would be cruel, no?
+# Who knows
+# I'll probably just use a png and write a solver for it
+# Will be quicker and less annoying to use Entry boxes
+# I'll probably use a library for the png
+# I'll probably use PIL for the png
+# I'll probably use numpy for the solver
+# I'll probably use tkinter for the gui
+# I'll probably use pygame for the gui
+# No I'm not going schizophrenic
+# I'm just thinking out loud
+# I'm not even thinking out loud
+# I'm just typing out loud
+# I'm not even typing out loud
+# I'm just typing
+# I'm not even typing
+# I'm just pressing buttons
+# I'm not even pressing buttons
+# I'm just pressing keys
+# I'm not even pressing keys
+# I'm just pressing
+# I'm not even pressing
+# I'm just
+# I'm not even
+# I'm
+# I'm not
+# I
+
+# I'm not even sure what I'm doing anymore
+# Life MAN
+# If you know you know those emotes
+def getBoard():
+    ...
+
+def solveBoard(board):
+    board = getBoard()
+
+    
